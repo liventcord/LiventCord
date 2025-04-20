feecd98e8fa14a6a324a15ecc07515925854c6be
TheLp281 <160347604+TheLp281@users.noreply.github.com>
2025-04-20 21:36:55 +0300

    Fix config syntax

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -50,9 +50,8 @@ export default defineConfig(({ mode }) => {
           },
           entryFileNames: "assets/[name].js", 
           chunkFileNames: "assets/[name].js",
-          assetFileNames: "assets/[name].[ext]" 
+          assetFileNames: "assets/[name].[ext]"
         }
-      }
       }
     },
 
