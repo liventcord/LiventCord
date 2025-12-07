15dc33970d02482f5b608ce104b30d1ad81373c1
TheLp281 <efekantunc0@gmail.com>
2025-12-07 04:06:29 +0100

    Fix preview worker

--- server/cloudflare_workers/media-api/src/preview.ts
+++ server/cloudflare_workers/media-api/src/preview.ts
@@ -1,13 +1,15 @@
 export async function handlePreview(req: Request) {
   const ytApiUrl = "https://liventcord-media-api.onrender.com";
   const u = new URL(req.url);
-  const attachmentId = u.searchParams.get("attachmentId");
+
+  const parts = u.pathname.split("/");
+  const attachmentId = parts[2];
+
   if (!attachmentId || !/^[a-zA-Z0-9_-]+$/.test(attachmentId)) {
     return new Response("Invalid attachmentId", { status: 400 });
   }
-  const targetUrl = new URL(
-    `/attachments/${attachmentId}/preview`,
-    "https://" + ytApiUrl,
-  );
+
+  const targetUrl = new URL(`/attachments/${attachmentId}/preview`, ytApiUrl);
+
   return fetch(targetUrl.toString());
 }
