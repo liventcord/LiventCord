72f5516b2ac0c5719bec98bbff575e59aef0a428
Reeyuki <160347604+Reeyuki@users.noreply.github.com>
2025-12-14 21:20:52 +0100

    Modify NotFoundContent redirect logic
    Updated the NotFoundContent component to redirect using hash instead of search.

--- docs/patch404.js
+++ docs/patch404.js
@@ -19,20 +19,17 @@ const filePath = path.resolve('./node_modules/@docusaurus/theme-classic/lib/them
 
 const modifiedContent = `
 import React, { useEffect } from 'react';
-import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
-import Heading from '@theme/Heading';
 
 export default function NotFoundContent({ className }) {
   useEffect(() => {
-    const fullPath = window.location.pathname + window.location.search + window.location.hash;
-    window.location.href = "https://liventcord.github.io" + "/LiventCord/app?page=" + encodeURIComponent(fullPath);
+    const fullPath = window.location.pathname + window.location.hash;
+    window.location.href = "https://liventcord.github.io/LiventCord/app#" + encodeURIComponent(fullPath);
   }, []);
   
   return null;
 }
-
 `.trim()
+
 
 fs.writeFileSync(filePath, modifiedContent, 'utf-8')
 console.log('✅ Not found page updated with redirect logic.')
