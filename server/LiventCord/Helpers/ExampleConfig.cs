b40d2e68139be19fafc05c6d0829d5c0ee9b29b2
TheLp281 <efekantunc0@gmail.com>
2026-01-08 00:21:13 +0100

    Embed default configs inside example config

--- server/LiventCord/Helpers/ExampleConfig.cs
+++ server/LiventCord/Helpers/ExampleConfig.cs
@@ -1,16 +1,46 @@
-using Microsoft.Extensions.Configuration;
-
+using System.Text;
 public static class ExampleConfig
 {
     private static readonly IConfigurationRoot _config;
-
     static ExampleConfig()
     {
+        var json = @"
+        {
+        ""Logging"": {
+            ""LogLevel"": {
+            ""Default"": ""Warning"",
+            ""Microsoft.EntityFrameworkCore"": ""Error"",
+            ""Microsoft.EntityFrameworkCore.Database.Command"": ""Error""
+            }
+        },
+        ""AllowedHosts"": ""*"",
+        ""AppSettings"": {
+            ""DatabaseType"": ""sqlite"",
+            ""RemoteConnection"": ""Host=localhost;Port=5432;Username=myuser;Password=mypassword;Database=mydatabase;"",
+            ""SqlitePath"": ""Data/liventcord.db"",
+            ""SqliteCachePath"": ""Data/liventcord_cache.db"",
+            ""MinPoolSize"": ""1"",
+            ""MaxPoolSize"": ""3"",
+            ""Host"": ""0.0.0.0"",
+            ""Port"": ""5005"",
+            ""FrontendUrl"": ""http://localhost:3000"",
+            ""GifWorkerUrl"": ""https://liventcord-gif-worker.efekantunc0.workers.dev"",
+            ""MediaWorkerUrl"": ""YOUR_MEDIA_WORKER_URL"",
+            ""MaxAvatarSize"": ""3"",
+            ""MaxAttachmentsSize"": ""30"",
+            ""EnableMetadataIndexing"": true,
+            ""MetadataDomainLimit"": ""100"",
+            ""JwtKey"": ""YourSuperSecretKeyHereThatShouldBeAtLeast32BytesLong"",
+            ""AccessTokenExpiryDays"": 30
+        }
+        }";
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
         _config = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("Properties/exampleSettings.json", optional: false, reloadOnChange: true)
+            .AddJsonStream(stream)
             .Build();
     }
+
 
     /// <summary>
     /// Get a value from the AppSettings section and convert it to the requested type.
